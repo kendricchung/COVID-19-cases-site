e16f026374beb9f22921210fb100218585bf4d32
v9k1b <v9k1b@ugrad.cs.ubc.ca>
2020-05-09 16:05:46 -0700

    Updates

---
+++ precache-manifest.0ee3723343fdcfe46cbeef8d82b44c22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "373a704c30b2d7fc3dba2bc5e81adb0a",
+    "url": "/COVID-19-cases-site/index.html"
+  },
+  {
+    "revision": "5abd52c4c4360b1c6d4f",
+    "url": "/COVID-19-cases-site/static/css/main.958ca9aa.chunk.css"
+  },
+  {
+    "revision": "7e5b33f416dc82592c5b",
+    "url": "/COVID-19-cases-site/static/js/2.d9ae09d5.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19-cases-site/static/js/2.d9ae09d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5abd52c4c4360b1c6d4f",
+    "url": "/COVID-19-cases-site/static/js/main.2c7ab8f0.chunk.js"
+  },
+  {
+    "revision": "70263b3a5cdf30b1aed5",
+    "url": "/COVID-19-cases-site/static/js/runtime-main.6ae076e2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid_19_graph_app/precache-manifest.1263012a1ae247795b2adb20777b3ddb.js"
+  "/COVID-19-cases-site/precache-manifest.0ee3723343fdcfe46cbeef8d82b44c22.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covid_19_graph_app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/COVID-19-cases-site/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
