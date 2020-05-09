79e2de5b9c583530f108169f732c6072dd00fe54
v9k1b <v9k1b@ugrad.cs.ubc.ca>
2020-05-09 16:01:16 -0700

    changed to github icon

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Favicon from 'react-favicon';
 
 import App from './App';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(
+    <div>
+        <Favicon url="http://oflisback.github.io/react-favicon/public/img/github.ico" />
+        <App/>
+    </div>,
+document.getElementById('root'));
