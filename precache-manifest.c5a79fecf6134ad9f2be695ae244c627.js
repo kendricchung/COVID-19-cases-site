36448b846d0a7622614daf4b24484d03406e038a
v9k1b <v9k1b@ugrad.cs.ubc.ca>
2020-05-09 15:09:18 -0700

    Updates

---
+++ precache-manifest.c5a79fecf6134ad9f2be695ae244c627.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fd5e92d47af2c398aa5829aff80a763",
+    "url": "/covid_19_graph_app/index.html"
+  },
+  {
+    "revision": "09cff2f5df1f4a519a5c",
+    "url": "/covid_19_graph_app/static/css/main.b1fa1546.chunk.css"
+  },
+  {
+    "revision": "d1f58968907997235bf4",
+    "url": "/covid_19_graph_app/static/js/2.c6848210.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid_19_graph_app/static/js/2.c6848210.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09cff2f5df1f4a519a5c",
+    "url": "/covid_19_graph_app/static/js/main.f01264d8.chunk.js"
+  },
+  {
+    "revision": "23400fabc67d2d9d2fda",
+    "url": "/covid_19_graph_app/static/js/runtime-main.4506967c.js"
+  }
+]);
